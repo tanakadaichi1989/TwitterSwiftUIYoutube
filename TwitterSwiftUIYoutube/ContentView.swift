d3ff9725562369f5952047a3b772b6b155c54a4f
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-04-23 18:24:35 +0900

    Initial Commit

---
+++ TwitterSwiftUIYoutube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by 田中大地 on 2022/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUIYoutube/TwitterSwiftUIYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIYoutubeApp.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by 田中大地 on 2022/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
