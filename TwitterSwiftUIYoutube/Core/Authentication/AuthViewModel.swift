519c173781fba4eea0a7b7cc8650e2229525a573
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-02 16:41:35 +0900

    Implement Register and Login Function using Firebase

--- TwitterSwiftUIYoutube/Core/Authentication/AuthViewModel.swift
+++ TwitterSwiftUIYoutube/Core/Authentication/AuthViewModel.swift
@@ -13,14 +13,50 @@ class AuthViewModel: ObservableObject {
     
     init(){
         self.userSession = Auth.auth().currentUser
-        print("DEBUG: User session is \(self.userSession)")
+        print("DEBUG: User session is \(self.userSession?.uid)")
     }
     
     func login(withEmail email: String, password: String){
-        print("DEBUG: Login with \(email)")
+        Auth.auth().signIn(withEmail: email, password: password){ result, error in
+            if let error = error {
+                print("DEBUG: Failed to sign in with \(error.localizedDescription)")
+                return
+            }
+            
+            guard let user = result?.user else { return }
+            self.userSession = user
+            print("DEBUG: Did log user in ...")
+        }
     }
     
     func register(withEmail email: String, password: String, fullname:String, username:String){
-        print("DEBUG: Register with \(email)")
+        Auth.auth().createUser(withEmail: email, password: password){ result, error in
+            if let error = error  {
+                print("DEBUG: Failed to register with \(error.localizedDescription)")
+                return
+            } else {
+                guard let user = result?.user else { return }
+                self.userSession = user
+                
+                print("DEBUG: Registered user successfully")
+                print("DEBUG: User is \(self.userSession)")
+                
+                let data = ["email": email,
+                            "username": username.lowercased(),
+                            "fullname": fullname,
+                            "uid": user.uid]
+                
+                Firestore.firestore().collection("users")
+                    .document(user.uid)
+                    .setData(data){ _ in
+                        print("DEBUG: Did upload user data ...")
+                    }
+            }
+        }
+    }
+    
+    func signOut(){
+        userSession = nil
+        // try? Auth.auth().signOut()
     }
 }
