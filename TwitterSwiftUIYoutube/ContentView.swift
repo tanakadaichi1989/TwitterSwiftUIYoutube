eee22bc449810f02bcd691babd1bc5270e2a2e9f
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-12 01:29:43 +0900

    Show User Profile Image on ContentView using Package Kingfisher

--- TwitterSwiftUIYoutube/ContentView.swift
+++ TwitterSwiftUIYoutube/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ContentView: View {
     @State private var showMenu = false
@@ -56,13 +57,18 @@ extension ContentView {
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading){
-                Button {
-                    withAnimation(.easeInOut) {
-                         showMenu.toggle()
+                if let user = viewModel.currentUser {
+                    Button {
+                        withAnimation(.easeInOut) {
+                            showMenu.toggle()
+                        }
+                    } label :{
+                        KFImage(URL(string: user.profileImageUrl))
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(Circle())
+                            .frame(width: 32, height: 32)
                     }
-                } label :{
-                    Circle()
-                        .frame(width: 32, height: 32)
                 }
             }
         }
