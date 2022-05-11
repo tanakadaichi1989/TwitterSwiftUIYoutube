627f9b1b2bbc4be5dba13ce4397ce689077b347c
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-12 01:25:19 +0900

    Show User Profile Image on SideMenuView using Package Kingfisher

--- TwitterSwiftUIYoutube/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterSwiftUIYoutube/Core/SideMenu/Views/SideMenuView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct SideMenuView: View {
     @EnvironmentObject var viewModel: AuthViewModel
@@ -15,7 +16,10 @@ struct SideMenuView: View {
             VStack(alignment: .leading, spacing: 32) {
                 //sideMenuHeader
                 VStack(alignment: .leading) {
-                    Circle()
+                    KFImage(URL(string: user.profileImageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(Circle())
                         .frame(width: 48, height: 48)
                     
                     VStack(alignment: .leading, spacing: 4){
