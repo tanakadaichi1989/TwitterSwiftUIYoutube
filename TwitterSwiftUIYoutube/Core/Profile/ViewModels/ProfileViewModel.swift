dfa786005326dfffb494dd7086d77954674754a6
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-12 23:36:28 +0900

    Implement Fetch User Tweets for Profile Fucntion

---
+++ TwitterSwiftUIYoutube/Core/Profile/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewModel.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by 田中大地 on 2022/05/12.
+//
+
+import Foundation
+
+class ProfileViewModel: ObservableObject {
+    @Published var tweets = [Tweet]()
+    private let service = TweetService()
+    let user: User
+    
+    init(user: User){
+        self.user = user
+        self.fetchUserTweets()
+    }
+    
+    func fetchUserTweets(){
+        guard let uid = user.id else { return }
+        service.fetchTweets(forUid: uid) { tweets in
+            self.tweets = tweets
+            
+            for i in 0 ..< tweets.count {
+                self.tweets[i].user = self.user
+            }
+        }
+    }
+}
--- TwitterSwiftUIYoutube/Service/TweetService.swift
+++ TwitterSwiftUIYoutube/Service/TweetService.swift
@@ -31,11 +31,22 @@ struct TweetService {
     
     func fetchTweets(completion: @escaping([Tweet]) -> Void) {
         Firestore.firestore().collection("tweets")
-            .order(by: "timestamp", descending: false)
+            .order(by: "timestamp", descending: true)
             .getDocuments { snapshot, _ in
                 guard let documents = snapshot?.documents else { return }
-                let tweet = documents.compactMap({ try? $0.data(as: Tweet.self)})
-                completion(tweet)
+                let tweets = documents.compactMap({ try? $0.data(as: Tweet.self)})
+                completion(tweets)
+            }
+    }
+    
+    func fetchTweets(forUid uid: String, completion: @escaping([Tweet]) -> Void) {
+        Firestore.firestore().collection("tweets")
+            //.whereField("uid",isEqualTo: uid)
+            .order(by: "timestamp", descending: true)
+            .getDocuments { snapshot, _ in
+                guard let documents = snapshot?.documents else { return }
+                let tweets = documents.compactMap({ try? $0.data(as: Tweet.self)})
+                completion(tweets.sorted(by: { $0.timestamp.dateValue() > $1.timestamp.dateValue() }))
             }
     }
 }
