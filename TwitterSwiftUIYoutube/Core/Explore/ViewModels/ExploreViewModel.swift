a393c521e405e51cfcdbb1c58649abfefbee9dfd
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-12 21:29:11 +0900

    Implement Search User Function

--- TwitterSwiftUIYoutube/Core/Explore/ViewModels/ExploreViewModel.swift
+++ TwitterSwiftUIYoutube/Core/Explore/ViewModels/ExploreViewModel.swift
@@ -9,6 +9,20 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
+    
+    var searchBarUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            
+            return users.filter {
+                $0.username.contains(lowercasedQuery) || $0.fullname.lowercased().contains(lowercasedQuery)
+            }
+        }
+    }
+    
     let service = UserService()
     
     init(){
