5ab65aa9e225913c9d926095e6f3c1083584ad74
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-13 21:55:36 +0900

    Implement Like Function

---
+++ TwitterSwiftUIYoutube/Core/Components/Tweets/ViewModel/TweetRowViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  TweetViewModel.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by 田中大地 on 2022/05/13.
+//
+
+import Foundation
+
+class TweetRowViewModel: ObservableObject {
+    @Published var tweet: Tweet
+    private let service = TweetService()
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+    }
+    
+    func likeTweet() {
+        service.likeTweet(tweet) {
+            self.tweet.didLike = true
+        }
+    }
+    
+    func unlikeTweet() {
+        service.likeTweet(tweet) {
+            self.tweet.didLike = false
+        }
+    }
+    
+    func checkIfUserLikedTweet() {
+        service.checkIfUserLikedTweet(tweet) { didLike in
+            if didLike {
+                self.tweet.didLike = true
+            }
+        }
+    }
+    
+}
--- TwitterSwiftUIYoutube/Service/TweetService.swift
+++ TwitterSwiftUIYoutube/Service/TweetService.swift
@@ -7,6 +7,7 @@
 
 import Firebase
 import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct TweetService {
     
@@ -47,6 +48,48 @@ struct TweetService {
                 guard let documents = snapshot?.documents else { return }
                 let tweets = documents.compactMap({ try? $0.data(as: Tweet.self)})
                 completion(tweets.sorted(by: { $0.timestamp.dateValue() > $1.timestamp.dateValue() }))
+            }
+    }
+    
+    func likeTweet(_ tweet: Tweet, completion: @escaping() -> Void) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let tweetId = tweet.id else { return }
+        
+        let userLikeRef = Firestore.firestore().collection("users").document(uid).collection("user-likes")
+        
+        Firestore.firestore().collection("tweets").document(tweetId)
+            .updateData(["likes" : tweet.likes + 1]) { _ in
+                userLikeRef.document(tweetId).setData([:]) { _ in
+                    completion()
+                }
+            }
+    }
+    
+    func unlikeTweet(_ tweet: Tweet, completion: @escaping() -> Void){
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let tweetId = tweet.id else { return }
+        guard tweet.likes > 0 else { return }
+        
+        let userLikeRef = Firestore.firestore().collection("users").document(uid).collection("user-likes")
+        
+        Firestore.firestore().collection("tweets").document(tweetId)
+            .updateData(["likes": tweet.likes - 1]) { _ in
+                userLikeRef.document(tweetId).delete { _ in
+                    completion()
+                }
+            }
+    }
+    
+    func checkIfUserLikedTweet(_ tweet: Tweet, completion: @escaping(Bool) -> Void) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let tweetId = tweet.id else { return }
+        
+        Firestore.firestore().collection("users")
+            .document(uid)
+            .collection("user-likes")
+            .document(tweetId).getDocument { snapshot, _ in
+                guard let snapshot = snapshot else { return }
+                completion(snapshot.exists)
             }
     }
 }
